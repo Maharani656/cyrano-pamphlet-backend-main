d74dd4b47821a91e6db26f408b38cd75fb330d37
maharani656 <raniyuki65@gmail.com>
2025-08-13 03:09:10 +0300

    changed api file

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,23 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://cyrano-pamphlet.vercel.app/',
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'http://localhost:1338',
+        'http://localhost:1339',
+        'http://localhost:1340',
+        'http://localhost:1341',
+        'http://localhost:1342',
+      ],
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
